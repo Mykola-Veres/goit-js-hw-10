cc872fd178fda4e8012cb331fa029d3ef3ba1c34
Mykola-Veres <veresnickolay@gmail.com>
2021-11-14 10:39:14 +0200

    new

--- templates/test.js
+++ templates/test.js
@@ -2,3 +2,9 @@
 // const markup = countryUser.map(allCountries).join("");
 // countryList.innerHTML = markup;
 // const values = Object.values(languages);
+
+// let a = 5;
+// let b = "Hello";
+
+// [a,b] = [b,a];
+// console.log(a, b)
